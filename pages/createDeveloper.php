39066ce9da635d9b5dcf8ba9356143c422d5b379
Beatrice Tan <beatricetanjt@gmail.com>
2023-06-27 12:07:49 +0800

    Create developer from applications page

---
+++ pages/createDeveloper.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create New Developer</title>
+</head>
+<body>
+    <h1>Create Developer</h1>
+    <form id="addDeveloperForm">
+        Developer Name: <input type="text" name="name"><br>
+        <input type="submit" value="Create Developer Post">
+    </form>
+    <div id="statusMessage"></div>
+</body>
+</html>
+
+<script src="../js/manage-applications.js"></script>
