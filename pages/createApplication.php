6eb6f5274cc7097e7173fbb3dbca1156cfbf0c25
Beatrice Tan <beatricetanjt@gmail.com>
2023-06-27 13:06:27 +0800

    changes

---
+++ pages/createApplication.php
@@ -0,0 +1,21 @@
+<?php
+    require_once('./header.php');
+?>
+
+<body>
+    <h2>Create an application post</h2>
+    <form id="addApplicationForm">
+        Title <br>
+        <input type="text" name="appTitle" placeholder="Add a title"><br>
+        Description <br> 
+        <input type="text" name="appDescription" placeholder="Add a description"><br><br>
+        <input type="hidden" name="developerID" value="">
+        <input type="submit" value="Next">
+    </form>
+
+    <div id="statusMessage"></div>
+</body>
+
+</html>
+
+<script src="../js/manage-applications.js"></script>
