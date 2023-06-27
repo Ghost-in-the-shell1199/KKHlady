468f1089bcb7ff9f1f3c8104628176e036d96e2f
Vishnu <atchayakumaran3@gmail.com>
2023-06-27 12:33:43 +0800

    css and header
    css and header

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<div class="user">
+    Hello Admin
+    <img class="profile" src="images/image-removebg-preview.png" width="20px">
+</div>
+<header>
+    <h3>
+        Department of children Development(DCD)<br>
+        KK Women's and Children Hospital
+    </h3>
+    <nav>
+        <ul class="menu">
+            <li><a href="login.php">Dashboard</a></li>
+            <li><a href="login.php">Applications</a></li>
+            <li><a href="login.php">Application Manager</a></li>
+            <li><a href="login.php">Logout</a></li>
+
+        </ul>
+    </nav>
+</header>
+<body>
+    <p>login in as</p>
+    <img src="developer" alt="developer">
+    <h3>developers<h3>
+    <img src="admin" alt="admin">
+    <h3>admin<h3>
+</body>
+</html>
