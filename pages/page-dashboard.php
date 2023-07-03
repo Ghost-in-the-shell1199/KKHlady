a13800814da6faffd3189a85b54e1f3c4869ab01
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-07-03 12:08:39 +0800

    buttons

--- pages/page-dashboard.php
+++ pages/page-dashboard.php
@@ -6,14 +6,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/dashboard.css">
     <title>Document</title>
 </head>
 <body>
 <div class="w3-content w3-display-container">
   <img class="mySlides" src="https://www.asiaone.com/sites/default/files/original_images/Aug2022/170822_Uncle%20Raymond_TikTok_0.jpg" style="width:50%">
-  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/photo_2022-05-13-16.32.39.jpeg" style="width:50%">
-  <img class="mySlides" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSNnMZP1IT2KFDaXW5ARrFhpCchLUTxB6N-A&usqp=CAU" style="width:70%">
-  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/IMG_5184.jpg" style="width:70%">
+  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/photo_2022-05-13-16.32.39.jpeg" style="width:25%">
+  <img class="mySlides" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSNnMZP1IT2KFDaXW5ARrFhpCchLUTxB6N-A&usqp=CAU" style="width:25%">
+  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/IMG_5184.jpg" style="width:25%">
   
   <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
   <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
