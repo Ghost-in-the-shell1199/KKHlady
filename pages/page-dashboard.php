ac3ba15ebed08e9165785b88bb14bc9492e93767
jawdonchan <118231622+jawdonchan@users.noreply.github.com>
2023-07-03 12:03:54 +0800

    fixed?

--- pages/page-dashboard.php
+++ pages/page-dashboard.php
@@ -10,10 +10,10 @@
 </head>
 <body>
 <div class="w3-content w3-display-container">
-  <img class="mySlides" src="img_snowtops.jpg" style="width:100%">
-  <img class="mySlides" src="img_lights.jpg" style="width:100%">
-  <img class="mySlides" src="img_mountains.jpg" style="width:100%">
-  <img class="mySlides" src="img_forest.jpg" style="width:100%">
+  <img class="mySlides" src="https://www.asiaone.com/sites/default/files/original_images/Aug2022/170822_Uncle%20Raymond_TikTok_0.jpg" style="width:50%">
+  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/photo_2022-05-13-16.32.39.jpeg" style="width:50%">
+  <img class="mySlides" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSNnMZP1IT2KFDaXW5ARrFhpCchLUTxB6N-A&usqp=CAU" style="width:70%">
+  <img class="mySlides" src="https://theurbanwire.com/wp-content/uploads/2022/05/IMG_5184.jpg" style="width:70%">
   
   <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
   <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
