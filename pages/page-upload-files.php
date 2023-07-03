34a614df863f068bc358f8ac64794b6c890f21de
Ghost-in-the-shell1199 <118423706+Ghost-in-the-shell1199@users.noreply.github.com>
2023-07-03 10:14:46 +0800

    more changes

--- pages/page-upload-files.php
+++ pages/page-upload-files.php
@@ -18,65 +18,24 @@
 body {
   font-family: Arial, Helvetica, sans-serif;
 }
-.dropbtn {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-.navbar a:hover, .dropdown:hover .dropbtn {
-  background-color: red;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
 </style>
 <body>
 <h1>upload</h1>
 <div class="center">
-    <h3>location</h3>
-    <div class="dropdown">
-    <button class="dropbtn">Dropdown 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div>
+  <form action="/action_page.php">
+  <label for="files">location</label>
+  <select id="files" name="files">
+    <option value="files1">files1</option>
+    <option value="file2">file2</option>
+    <option value="file3">file3</option>
+    <option value="file4">file4</option>
+  </select>
+</form>
   <form action="/page-upload-files.php">
   <input type="file" id="myFile" name="filename">
   <input type="submit">
 </form>
+
 </div>
 </body>
 <?php
