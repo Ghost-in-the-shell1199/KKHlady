8ce199b28fe6d26f649442f5d9b4390335232f8c
Vishnu <atchayakumaran3@gmail.com>
2023-07-16 01:08:38 +0800

    edited
    edited

--- pages/page-create-developer.php
+++ pages/page-create-developer.php
@@ -1,11 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/createDeveloper.css">
+</head>
+
 <?php
     require_once('./page-header.php');
 ?>
 <body>
     <h1>Create Developer</h1>
     <form id="addDeveloperForm">
-        Developer Name: <input type="text" name="name"><br>
-        <input type="submit" value="Create Developer Post">
+         Name: <input class="name" type="text" name="name" placeholder="Developer Name"><br>
+        <input class="button" type="submit" value="Create Developer Post">
     </form>
     <div id="statusMessage"></div>
 </body>
